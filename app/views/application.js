8a2d21c5a5bba46bb72dc0a135613c9e736a53a8
Rodney Folz <rodney@rodneyfolz.com>
2014-12-08 21:06:19 -0500

    Remove unused var and add global declaration in appview.

--- app/views/application.js
+++ app/views/application.js
@@ -1,8 +1,9 @@
+/* global hljs */
+
 import Ember from 'ember';
 
 export default Ember.View.extend({
   didInsertElement: function() {
-    var that = this;
     Ember.run.schedule('afterRender', function() {
       hljs.initHighlightingOnLoad();
     });
